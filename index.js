1c8ca4e9419dd74810c113b981553992628ed904
Llewelyn <lif@arcwave.co.uk>
2021-11-08 17:58:32 +0000

    Intitial

---
+++ index.js
@@ -0,0 +1,29 @@
+// Import the modules we need
+var express = require ('express')
+var ejs = require('ejs')
+var bodyParser= require ('body-parser')
+
+// Create the express application object
+const app = express()
+const port = 8000
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Set the directory where Express will pick up HTML files
+// __dirname will get the current directory
+app.set('views', __dirname + '/views');
+
+// Tell Express that we want to use EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tells Express how we should process html files
+// We want to use EJS's rendering engine
+app.engine('html', ejs.renderFile);
+
+// Define our data
+var shopData = {shopName: "Bertie's Books"}
+
+// Requires the main.js file inside the routes folder passing in the Express app and data as arguments.  All the routes will go in this file
+require("./routes/main")(app, shopData);
+
+// Start the web app listening
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ routes/main.js
@@ -0,0 +1,24 @@
+module.exports = function(app, shopData) {
+
+    // Handle our routes
+    app.get('/',function(req,res){
+        res.render('index.ejs', shopData)
+    });
+    app.get('/about',function(req,res){
+        res.render('about.ejs', shopData);
+    });
+    app.get('/search',function(req,res){
+        res.render("search.ejs", shopData);
+    });
+    app.get('/search-result', function (req, res) {
+        //searching in the database
+        res.send("You searched for: " + req.query.keyword);
+    });
+    app.get('/register', function (req,res) {
+        res.render('register.ejs', shopData);                                                                     
+    });                                                                                                 
+    app.post('/registered', function (req,res) {
+        // saving data in database
+        res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                              
+    }); 
+}
